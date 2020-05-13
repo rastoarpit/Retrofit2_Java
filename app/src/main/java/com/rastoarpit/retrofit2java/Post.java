7530a62f5e2ac32b1eb335b8cbd5b46d379042a5
Arpit Rastogi <arpit.rastogi@yahoo.com>
2020-05-13 14:38:34 +0530

    Retrofit Tutorial Part 1 - SIMPLE GET REQUEST

---
+++ app/src/main/java/com/rastoarpit/retrofit2java/Post.java
@@ -0,0 +1,36 @@
+package com.rastoarpit.retrofit2java;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Post {
+
+
+    @SerializedName("userId")
+    private int userId;
+
+    @SerializedName("id")
+    private int id;
+
+    @SerializedName("title")
+    private String title;
+
+    @SerializedName("body")
+    private String text;
+
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
